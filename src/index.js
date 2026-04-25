8b8d1ef4b661385f2ade190bce7652c1eb2bdfda
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-04-25 13:40:09 +0300

    Добавил файл index.js и добавил файлы для билда

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Team} from "./Team.js";
+import {ErrorRepository} from "./ErrorRepository.js";
+import {Settings} from "./Settings.js";
+
+new Team();
+new ErrorRepository();
+new Settings();
